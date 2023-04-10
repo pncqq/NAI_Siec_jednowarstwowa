86adf39e4437b2343f834ee5776da97b1b5baa05
Filip Michalski <s24916@pjwstk.edu.pl>
2023-04-10 21:57:44 +0200

    Wszystko, oprócz opcjonalnego punktu

--- functions.py
+++ functions.py
@@ -9,6 +9,7 @@ import random as rnd
 label_to_numeric = {}
 dimension_count = 0
 letters_count = 26
+all_normalized_vectors = []
 
 
 # Wczytywanie danych z pliku CSV
@@ -45,13 +46,31 @@ def random_weight_matrix():
     return np.random.rand(dimension_count, letters_count)
 
 
+# Generuje bias
 def random_bias_matrix():
     return np.random.rand(dimension_count, 1)
 
 
-# Generuje wekto znormalizowany
+# Generuje wektor znormalizowany
 def text_to_vector(text_with_labels):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    # Jeśli to tylko jeden tekst
+    if isinstance(text_with_labels, str):
+        # Tworzymy słownik liczników, jeden dla każdej litery alfabetu
+        counts = collections.defaultdict(int)
+        for char in text_with_labels.lower():
+            # Jeśli litera jest w alfabecie, zwiększamy jej licznik o 1
+            if char in alphabet:
+                counts[char] += 1
+
+        # Zamieniamy słownik na wektor i normalizujemy go
+        vector = [counts[letter] for letter in alphabet]
+        total = sum(vector)
+
+        # Normalizujemy wektor (czestotliwosc wystapien danej liczby w kolejnosci alfabetycznej)
+        normalized_vector = [count / total for count in vector]
+        return normalized_vector
 
     # Tworzymy listę wektorów dla każdego tekstu
     vectors = []
@@ -80,6 +99,9 @@ def text_to_vector(text_with_labels):
             normalized_vector.append(text_with_labels[i][1])
 
             vectors.append(normalized_vector)
+
+    global all_normalized_vectors
+    all_normalized_vectors = vectors
     return vectors
 
 
@@ -88,14 +110,78 @@ def activation_function(net):
     return net
 
 
-def learn(input_vectors, weight_matrix, alpha, bias):
+# Uczenie
+def learn(input_vectors, weight_matrix, alpha, bias, max_err):
+    E = 1
+
+    while E > max_err:
+        # Przechodzimy przez wektory wejsciowe
+        for vec in input_vectors:
+            # Obliczamy W * x
+            x = np.array(vec[:-1])
+            Wx = np.matmul(weight_matrix, x)
+            # Odejmujemy bias (funkcja liniowa, wiec net = f(net))
+            biasT = np.transpose(bias)
+            # Klasyfikacja
+            net = Wx - biasT
+
+            # Rozszerzamy oczekiwaną wartość wyjściową na macierz
+            d = vec[-1]
+            d_matrix = np.zeros(4)
+            if d == 0:  # GR
+                d_matrix[0] = 1
+            elif d == 1:  # PL
+                d_matrix[1] = 1
+            elif d == 2:  # EN
+                d_matrix[2] = 1
+            else:  # SP
+                d_matrix[3] = 1
+
+            # Obliczamy miarę błędu
+            diff = d_matrix - net
+            E = 0.5 * np.dot(diff, np.transpose(diff))
+            E = E[0][0]
+
+            # Aktualizacja wag metodą gradientową
+            # f'(net) = 1 -> bo liniowa
+            diff_matrix = alpha * diff * 1
+            diff_matrix = diff_matrix[0].tolist()
+            diff_matrix = np.array(diff_matrix)
+            diff_matrix = diff_matrix.reshape((4, 1))
+
+            x = x.tolist()
+            x = np.array(x)
+            x = x.reshape((1, 26))
+
+            # Mnożenie macierzy
+            matrix = np.dot(diff_matrix, x)
+
+            new_weights = weight_matrix + matrix
+            weight_matrix = new_weights
+
+            # Aktualizacja bias
+            new_bias = bias - diff_matrix
+            bias = new_bias
+
+        print("Sieć nauczona!")
+        print(f"Miara błędu wynosi: {E}")
+    return weight_matrix, bias
+
+
+# Testowanie
+def test(input_vectors, weight_matrix, bias):
+    good = 0
+    all = len(input_vectors)
+    bad_vectors = []
+
     # Przechodzimy przez wektory wejsciowe
     for vec in input_vectors:
         # Obliczamy W * x
         x = np.array(vec[:-1])
         Wx = np.matmul(weight_matrix, x)
-        # Odejmujemy bias (funkcja liniowa, wiec net = f(net))
+        # Odejmujemy bias (funkcja liniowa, wiec net = f(net)) - WARTOŚĆ NET
         biasT = np.transpose(bias)
+        # Klasyfikacja
         net = Wx - biasT
 
         # Rozszerzamy oczekiwaną wartość wyjściową na macierz
@@ -110,26 +196,44 @@ def learn(input_vectors, weight_matrix, alpha, bias):
         else:  # SP
             d_matrix[3] = 1
 
-        # Obliczamy miarę błędu
-        diff = d_matrix - net
-        E = 0.5 * np.dot(diff, np.transpose(diff))
-        E = E[0][0]
+        # Sprawdzanie klasyfikacji
+        classified = np.argmax(net)
+        real_lang = np.argmax(d_matrix)
+        if classified == real_lang:
+            good = good + 1
+        else:
+            bad_vectors.append(vec)
 
-        # Aktualizacja wag metodą gradientową
-        # f'(net) = 1 -> bo liniowa
-        diff_matrix = alpha * diff * 1
-        diff_matrix = diff_matrix[0].tolist()
-        diff_matrix = np.array(diff_matrix)
-        diff_matrix = diff_matrix.reshape((4, 1))
+    print("Sieć przetestowana!")
+    acc = good / all * 100
+    print(f"Dokładność wynosi: {acc}%")
 
-        x = x.tolist()
-        x = np.array(x)
-        x = x.reshape((1, 26))
+    return bad_vectors
 
-        # Mnożenie macierzy
-        matrix = np.dot(diff_matrix, x)
 
-        new_weights = weight_matrix + matrix
+# Klasyfikacja jednego tekstu
+def classify(vec, weight_matrix, bias):
+    # Obliczamy W * x
+    x = np.array(vec)
+    Wx = np.matmul(weight_matrix, x)
+    # Odejmujemy bias (funkcja liniowa, wiec net = f(net)) - WARTOŚĆ NET
+    biasT = np.transpose(bias)
+    # Klasyfikacja
+    net = Wx - biasT
 
-        # Aktualizacja bias
-        new_bias = bias - diff_matrix
+    # Sprawdzanie klasyfikacji
+    print("Wykryto język:")
+    classified = np.argmax(net)
+    if classified == 0:
+        print("Niemiecki")
+    elif classified == 1:
+        print("Polski")
+    elif classified == 2:
+        print("Angielski")
+    elif classified == 3:
+        print("Hiszpański")
+
+
+def print_bad_vectors(bad_vectors):
+    # TODO: wypisać te teksty ze zbioru testowego, dla których klasyfikacja była błędna.
+    pass
--- main.py
+++ main.py
@@ -1,40 +1,4 @@
-import numpy as np
-
 import functions
-
-
-def train_perceptron(inputs, labels, alpha, epochs, bias=np.zeros(4), weights=np.zeros((26, 4))):
-    # pętla treningowa
-    for epoch in range(epochs):
-        # inicjujemy sumaryczny błąd na 0
-        total_error = 0.0
-
-        # przechodzimy po każdym wektorze wejściowym i etykiecie
-        for input_vector, label in zip(inputs, labels):
-            # obliczamy sumę iloczynów wag i wejść oraz dodajemy odchylenie
-            net = np.dot(input_vector, weights) + bias
-
-            # obliczamy wyjście neuronu
-            output = functions.activation_function(net)
-
-            # obliczamy błąd dla aktualnego wektora wejściowego i etykiety
-            error = label - output
-
-            # obliczamy zmianę wag na podstawie reguły delta
-            delta_weights = np.outer(input_vector, alpha * error)
-            delta_bias = alpha * error
-
-            # aktualizujemy wagi i bias
-            weights += delta_weights[:, np.newaxis]
-            bias += delta_bias
-
-            # dodajemy błąd do sumarycznego błędu
-            total_error += np.sum(np.abs(error))
-
-        # drukujemy informację o błędzie dla każdej epoki
-        print(f"Epoka {epoch + 1}: błąd = {total_error:.3f}")
-
-    return weights, bias
 
 
 def main():
@@ -56,7 +20,18 @@ def main():
     alpha = 1
 
     # Uczenie
-    functions.learn(vec1, weight_matrix, alpha, bias)
+    max_err1 = 0.07
+    weight_matrix, bias = functions.learn(vec1, weight_matrix, alpha, bias, max_err1)
+    print("\n")
+
+    # Testowanie
+    bad_vectors = functions.test(vec2, weight_matrix, bias)
+    functions.print_bad_vectors(bad_vectors)
+
+    # Klasyfikacja z konsoli
+    print("\n")
+    one_text = functions.text_to_vector(input("Wpisz tekst w danym języku: "))
+    functions.classify(one_text, weight_matrix, bias)
 
 
 if __name__ == '__main__':
